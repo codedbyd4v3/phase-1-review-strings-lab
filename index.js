9963cd9bb7d36f33876257e8d6441a8edb527958
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-09-23 12:03:17 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+
+const currentUser=`John`;
+const welcomeMessage=` Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage=`WELCOME TO FLATBOOK, JOHN!`;
+const shortGreeting=`Welcome, J !`;
